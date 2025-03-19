1bdf6df5b8e28268ff5ab4cc97c6b8654d1f8476
Revelin911 <revelin911@mail.com>
2025-03-18 21:35:03 -0600

    beginning questions values

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,7 +1,20 @@
-function quizQuestions({}) {
+import React from "react";
+
+const questions = [
+    {
+        _id: '',
+  question: '',
+  answers: [{
+    text: '',
+  isCorrect: '',
+  }],
+    }
+]
+
+// function quizQuestions({}) {
     
-    return(
-        <h2 data-cy ='coundown'> Guess Remaining: {{guesses}}</h2>
-        <h3 data-cy='countdown'
-    )
-}
+//     return(
+//         <h2 data-cy ='coundown'> Guess Remaining: {{guesses}}</h2>
+//         <h3 data-cy='countdown'
+//     )
+// }
