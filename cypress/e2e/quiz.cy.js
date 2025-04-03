e4d550c6e258d155af5b231e654b7357fefc4f1e
Revelin911 <revelin911@mail.com>
2025-04-03 17:23:33 -0600

    revised e2e to take pyhtonQuestions

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,15 +1,15 @@
 describe('Quiz End-to-End Test', () => {
     beforeEach(() => {
         cy.visit('/');
-        
+        cy.fixture('questions.json').as('questions');
     });
 
     it('starts the quiz when the Start button is clicked', () => {
         cy.contains('Start Quiz').click();
-cy.get('.question').should('be.visible');
+cy.contains(this.questions[0].question).should('be.visible');
     });
 
-    it('should go through each question at a time', () => {
+    it('should go through all questions', () => {
         cy.contains('Start Quiz').click();
         cy.get('option').each(($el) => {
             cy.wrap($el).click();
