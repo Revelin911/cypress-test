87c630d45e7bf89fbb6611dc2b491fd24a0f2bfe
Revelin911 <revelin911@mail.com>
2025-04-02 20:43:13 -0600

    adding tests to quiz component

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,10 +9,12 @@ export default defineConfig({
       bundler: 'vite',
       viteConfig,
     },
+    specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
   },
 
   e2e: {
     baseUrl: 'http://localhost:3001',
+    supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,20 +1,49 @@
 import React from "react";
+import Quiz from '../../client/src/components/Quiz';
+import { mount } from 'cypress/react';
 
-const questions = [
-    {
-        _id: '',
-  question: '',
-  answers: [{
-    text: '',
-  isCorrect: '',
-  }],
-    }
-]
+describe('Quiz Component', () => {
+    it('should initially render the start button', () => {
+        cy.mount(<Quiz />);
+        cy.contains('Start Quiz').should('be.visible');
+    });
 
-// function quizQuestions({}) {
-    
-//     return(
-//         <h2 data-cy ='coundown'> Guess Remaining: {{guesses}}</h2>
-//         <h3 data-cy='countdown'
-//     )
-// }
+    it('starts quiz when the Start button is clicked', () => {
+        cy.mount(<Quiz />);
+        cy.contains('Start Quiz').click();
+        cy.get('1').should('be.visible');
+    });
+
+    it('continues to next question after answering', () => {
+        cy.mount(<Quiz />);
+        cy.contains('Start Quiz').click();
+        cy.get('2').should('have.text', 'answers');
+    });
+
+    it('continues to next question after answering', () => {
+        cy.mount(<Quiz />);
+        cy.contains('Start Quiz').click();
+        cy.get('3').should('have.text', 'answers');
+    });
+
+    it('continues to next question after answering', () => {
+        cy.mount(<Quiz />);
+        cy.contains('Start Quiz').click();
+        cy.get('4').should('have.text', 'answers');
+    });
+
+    it('continues to next question after answering', () => {
+        cy.mount(<Quiz />);
+        cy.contains('Start Quiz').click();
+        cy.get('5').should('have.text', 'answers');
+    });
+
+    it('ends the quiz and shows the final score', () => {
+        cy.mount(<Quiz />);
+        cy.contains('Start Quiz').click();
+
+        cy.get('option').should('exist').and('have.value', 't').click();
+    });
+
+    cy.contains('Your Score').should('be.visible');
+});
